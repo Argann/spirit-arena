5a8b59a3e0be80024382df43b8835609afc37640
Excilyano <agahnim13@gmail.com>
2018-10-27 00:14:37 +0200

    Spawner + basic IA

---
+++ Assets/Scripts/Mob_behaviour/MobManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobManager : MonoBehaviour {
+	public GameObject player;
+	private static GameObject staticPlayer;
+
+	// Use this for initialization
+	void Start () {
+		staticPlayer = player;
+	}
+
+	public static Vector2 GetPlayerPosition() {
+		return staticPlayer.transform.position;
+	}
+}
---
+++ Assets/Scripts/Mob_behaviour/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	public GameObject[] mobs_prefab;
+	public float delay = 5;
+	private float start_time = 0;
+	private float end_time;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+		end_time = Time.time;
+		if (end_time - start_time > delay) {
+			this.Spawn();
+			start_time = end_time;
+		}
+	}
+
+	void Spawn() {
+		int index = Random.Range(0, 6);
+		Instantiate(mobs_prefab[index]);
+	}
+}
