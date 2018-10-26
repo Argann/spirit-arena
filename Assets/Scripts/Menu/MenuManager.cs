7e204688a1f682351d469dba4cfd618aed466585
Argann Bonneau <argann.b@hotmail.fr>
2018-10-26 22:40:14 +0200

    Adding Main Menu Scene

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	[SerializeField]
+	private string creditsSceneName;
+
+	[SerializeField]
+	private string playSceneName;
+
+	public void ButtonQuit() {
+		Debug.Log("Goodbye !");
+		Application.Quit();
+	}
+
+	public void ButtonCredits() {
+		Debug.Log("Go To Credits ["+ this.creditsSceneName +"] !");
+		SceneManager.LoadScene(this.creditsSceneName);
+	}
+
+	public void ButtonPlay() {
+		Debug.Log("Go To Play ["+ this.playSceneName +"] !");
+		SceneManager.LoadScene(this.playSceneName);
+	}
+}
