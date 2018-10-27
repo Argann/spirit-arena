fdbc106f786399e3dc84f1da92fde343219b183f
Remi Laot <remilaot@gmail.com>
2018-10-27 11:20:20 -0700

    [feature/shots] shots have a timeout (set to 2000ms); players can control shot direction

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -9,6 +9,7 @@ public class PlayerControls : MonoBehaviour {
 	private Vector2 movement;
 
 	public float attackCooldown = 1;
+	public float attackSpeed = 10;
 	public float swapCooldown = 2;
 	private float attackStart;
 	private float swapStart;
@@ -24,19 +25,22 @@ public class PlayerControls : MonoBehaviour {
 	void Update () {
 		string horizontalInputLabel = string.Concat(playerPrefix, "_Horizontal");
 		string VerticalInputLabel   = string.Concat(playerPrefix, "_Vertical");
+		string aimHorizontalInputLabel = string.Concat(playerPrefix, "_aim_horizontal");
+		string aimVerticalInputLabel   = string.Concat(playerPrefix, "_aim_vertical");
 		string PlayerAction = string.Concat(playerPrefix, "_action");
 		string PlayerSwap = string.Concat(playerPrefix, "_swap");
 
-		float inputX = Input.GetAxisRaw(horizontalInputLabel);
-		float inputY = Input.GetAxisRaw(VerticalInputLabel);
-		movement = new Vector2(inputX, inputY);
+		movement = new Vector2(Input.GetAxisRaw(horizontalInputLabel), Input.GetAxisRaw(VerticalInputLabel));
 		movement.Normalize();
 		movement = movement * playerSpeed;
+
+		Vector2 aim = new Vector2(Input.GetAxis(aimHorizontalInputLabel), Input.GetAxis(aimVerticalInputLabel));
+		aim.Normalize();
 
 		if((Input.GetAxisRaw(PlayerAction) != 0) && (Time.time - attackStart > attackCooldown)) {
 			attackStart = Time.time;
 			GameObject instance = Instantiate(spells[state], transform.position, transform.rotation);
-			instance.GetComponent<Rigidbody2D>().velocity = new Vector2(3, 0);
+			instance.GetComponent<Rigidbody2D>().velocity = aim * attackSpeed;
 		}
 
 		if((Input.GetAxisRaw(PlayerSwap) != 0) && (Time.time - swapStart > swapCooldown)) {
---
+++ Assets/Scripts/Timeout.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timeout : MonoBehaviour {
+	public float ttlMillis;
+	private long startTime;
+
+	void Start () {
+		startTime = System.DateTime.Now.Ticks / System.TimeSpan.TicksPerMillisecond;
+	}
+	
+	void Update () {
+		if (System.DateTime.Now.Ticks / System.TimeSpan.TicksPerMillisecond >= startTime + ttlMillis)
+		{
+			Object.Destroy(this.gameObject);
+		}
+	}
+}
