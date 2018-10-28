d2d2f5227f2f63f1fbe671ade6c505dc77fa0b07
Remi Laot <remilaot@gmail.com>
2018-10-27 21:26:04 -0700

    [feature/weapon] weapons spawn randomly

--- Assets/Scripts/Weapons/WeaponManager.cs
+++ Assets/Scripts/Weapons/WeaponManager.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponManager : MonoBehaviour {
+	public GameObject[] weapons;
+	public long spawnTimerMs = 30000;
+	public long spawnTimerDeltaMs = 5000;
 
-	// Use this for initialization
-	void Start () {
-		
+	private long lastSpawnMs;
+	public long nextSpawnMs;
+
+	void Start() {
+		lastSpawnMs = System.DateTime.Now.Ticks / System.TimeSpan.TicksPerMillisecond;
+		nextSpawnMs = spawnTimerMs + (long)Random.Range(-spawnTimerDeltaMs, spawnTimerDeltaMs);
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        long now = System.DateTime.Now.Ticks / System.TimeSpan.TicksPerMillisecond;
+		if (now > lastSpawnMs + nextSpawnMs)
+		{
+			lastSpawnMs = now;
+			nextSpawnMs = spawnTimerMs + (long)Random.Range(-spawnTimerDeltaMs, spawnTimerDeltaMs);
+			int i = (int)(Random.value * weapons.Length);
+			float spawnY = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
+            float spawnX = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+            Instantiate(weapons[i], new Vector2(spawnX, spawnY), Quaternion.identity);
+		}
 	}
 }
+
+
+ 
