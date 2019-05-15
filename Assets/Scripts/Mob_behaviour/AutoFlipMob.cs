69d5464d9a6b00d91e078a134ccd63035c28244f
Argann Bonneau <argann.b@hotmail.fr>
2019-05-15 11:07:03 +0200

    Ajout du flip automatique des ennemis

---
+++ Assets/Scripts/Mob_behaviour/AutoFlipMob.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoFlipMob : MonoBehaviour {
+
+    private Vector2 previousPosition;
+
+    private bool isFacingLeft = true;
+
+    // Start is called before the first frame update
+    void Start() {
+        previousPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 currentPosition = transform.position;
+        if (previousPosition.x > currentPosition.x && !isFacingLeft) {
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            isFacingLeft = true;
+        } else if (previousPosition.x < currentPosition.x && isFacingLeft) {
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);                              
+            isFacingLeft = false;
+        }
+        previousPosition = transform.position;
+    }
+}
