2db17debfc02d66fc68e29f3b2850096b10aaa0c
Argann Bonneau <argann.b@hotmail.fr>
2019-05-15 11:56:54 +0200

    Switch des têtes de personnages UI

---
+++ Assets/Scripts/UI/AutoSwitchPlayerHead.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class AutoSwitchPlayerHead : MonoBehaviour {
+
+    public int playerNumber;
+    public Sprite spritePhysical;
+    public Sprite spriteSpirit;
+
+    private bool previousStateSpirit = true;
+    private PlayerControls currentPlayer;
+    private Image imageUI;
+
+    // Start is called before the first frame update
+    void Start() {
+        currentPlayer = MobManager.GetPlayer(playerNumber).GetComponent<PlayerControls>();
+        previousStateSpirit = currentPlayer.IsSpirit;
+
+        imageUI.sprite = currentPlayer.IsSpirit ? spriteSpirit : spritePhysical;
+
+
+        imageUI = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (currentPlayer.IsSpirit != previousStateSpirit) {
+            imageUI.sprite = currentPlayer.IsSpirit ? spriteSpirit : spritePhysical;
+            previousStateSpirit = currentPlayer.IsSpirit;
+        }
+    }
+}
