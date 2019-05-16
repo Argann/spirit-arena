5efbb48d5fdbb821da089382f979a0929b729050
Argann Bonneau <argann.b@hotmail.fr>
2019-05-16 17:33:36 +0200

    Clignotement des ennemis lorsqu'ils se font touchés

---
+++ Assets/Scripts/BlinkWhenHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkWhenHit : MonoBehaviour {
+
+    public List<Animator> animators = new List<Animator>();
+    
+    private MobCollider mobCollider;
+
+    private float previousLifePoints;
+
+    void Start() {
+        mobCollider = GetComponent<MobCollider>();
+
+        previousLifePoints = mobCollider.lifePoints;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (previousLifePoints > mobCollider.lifePoints) {
+            previousLifePoints = mobCollider.lifePoints;
+            foreach (Animator anim in animators) {
+                anim.SetTrigger("Hit");
+            }
+        }
+    }
+}
